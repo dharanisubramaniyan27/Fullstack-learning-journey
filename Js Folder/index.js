5a1b51b6b063f00b297f515994ef05c59a40441b
Dharani Subramaniyan <dharanisubramaniyan27@gmail.com>
2026-07-20 18:52:46 +0530

    Started JavaScript: internal/external scripts and printing methods

---
+++ Js Folder/index.js
@@ -0,0 +1,12 @@
+//....JS code
+
+//DOCUMENT.WRITE
+document.write("Hiii")
+document.write("<br>")
+document.write("Helloo")
+document.write("<br>")
+
+//CONSOLE.LOG
+console.log("Apple")
+console.log("Banana")
+console.log("Orange")
